f730b0f1326ae72cc80f363bf5b9c0b27df8853d
gokulbornforsuccess <48146826+gokulbornforsuccess@users.noreply.github.com>
2019-03-02 11:52:57 +0530

    Added scratch file
    Test the git environment.

---
+++ untitled2.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 19 07:22:54 2019
+
+@author: gokul
+"""
+#my first code
+print("Hello Python !!\ngokul") #comments
+
+#
+a=1;b=2;c=3
+d=e=f=1
+g,h,i=1,2,'abc123'
+
+d=d+1
+print(d,e,f)
+print(a,b,c)
+print(g,h,i)
+print(a)
+
+
+a='gokul'
+print(a[2:5]*5+'gokul')
+print(a[2])
+
+
+list1=['abc123',1234,'def234',5678]
+print (list1)
+print(list1[2:5])
+
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+
+"""
+gokul
+"""
